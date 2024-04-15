80984cde86c57acbc4ea182e6a56a18795aecd56
sokoliknet <42938723+sokoliknet@users.noreply.github.com>
2024-04-15 18:28:05 +0200

    first commit

---
+++ RabbitMQ.DirectExchange/Producer/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using RabbitMQ.Client;
+using System.Text;
+
+
+Task.Run(CreateTask(12000, "error"));
+Task.Run(CreateTask(10000, "info"));
+Task.Run(CreateTask(8000, "warning"));
+
+Console.ReadKey();
+
+static Func<Task> CreateTask(int timeToSleepTo, string routingKey)
+{
+    return () =>
+    {
+        var counter = 0;
+        do
+        {
+            int timeToSleep = new Random().Next(1000, timeToSleepTo);
+            Thread.Sleep(timeToSleep);
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+
+                string message = $"Message type [{routingKey}] from publisher N {counter}";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "direct_logs",
+                                    routingKey: routingKey,
+                                    basicProperties: null,
+                                    body: body);
+
+                Console.WriteLine($"Message type [{routingKey}] is sent into Direct Exchange [N:{counter++}]");
+            }
+        } while (true);
+    };
+}
